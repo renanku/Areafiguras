cfa3afc71192d442508e761fe8d61cfc23ea6072
lenovo <renanjesuskuchan12@gmail.com>
2024-09-26 11:02:23 -0600

    Actualizacion

--- AreaTri.java
+++ AreaTri.java
@@ -1,21 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class AreaTri {
-	public static void main(String [] args){
-        
-        double base, altura, resultado;     
-        Scanner obj = new Scanner(System.in);
-         
-        System.out.print("Ingrese base:\t");
-        base = obj.nextDouble();
-         
-        System.out.print("Ingrese altura:\t");
-        altura = obj.nextDouble();
-         
-        resultado = ((base*altura)/2);
-         
-        System.out.print("El area del Triangulo es:\t"+resultado);
-    }
-}
+public class Areatri {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Area = 0;
+		switch (Area) {
+		case 1 :
+			double base, altura, resultado;     
+	        Scanner obj = new Scanner(System.in);
+	         
+	        System.out.print("Ingrese base:\t");
+	        base = obj.nextDouble();
+	         
+	        System.out.print("Ingrese altura:\t");
+	        altura = obj.nextDouble();
+	         
+	        resultado = ((base*altura)/2);
+	         
+	        System.out.print("El area del Triangulo es:\t"+resultado);
+	        break;
+	        
+		case 2 :
+			
+			
+		case 3 :
+		}
+		
+		}
+	}
 
 
