db4fd3e31aa83ddd4e5086fd34aa0c45d6851350
Baltasogbi <baltasogbi@gmail.com>
2024-10-03 07:10:21 -0600

    Agregue el area del cuadrado

--- AreaTri.java
+++ AreaTri.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Areatri {
+public class AreaTri {
 
 
 	public static void main(String[] args) {
@@ -24,6 +24,12 @@ public class Areatri {
 	        break;
 	        
 		case 2 :
+			double lado1 = 0, lado2 = 0, resultado1;
+			Scanner obj1 = new Scanner (System.in);
+			System.out.print("Ingese el primer lado:\t");
+			System.out.print("ingrese el segundo lado:\t");
+			resultado1= (lado1*lado2);
+			System.out.print("El area del cuadrado es:\t"+ resultado1);
 			
 			
 		case 3 :
